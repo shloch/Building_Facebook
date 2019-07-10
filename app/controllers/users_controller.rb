a4317a583800c42341b39c44e0f87d6bfb50d8d2
SHEY LOUIS CHIA <shloch2007@yahoo.fr>
2019-07-10 16:38:14 +0100

    sanitizeing post+comment text

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+    include SessionsHelper
+    
+    def index
+    end
+  
+    def new
+    end
+ 
+    def show 
+    end
+  
+   
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   get 'posts/index'
   get 'posts/create'
   get 'posts/new'

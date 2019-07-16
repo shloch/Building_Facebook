2e11b71bc194f8cb134dba7d3ded2c86581be5fe
pwilson77 <p.o.yirenkyi@gmail.com>
2019-07-16 15:51:19 +0000

    modified migration file

--- db/migrate/20190701140111_create_friendships.rb
+++ db/migrate/20190701140111_create_friendships.rb
@@ -5,8 +5,8 @@ class CreateFriendships < ActiveRecord::Migration[5.2]
       
       t.timestamps
     end
-    add_reference :friendships , :inviting , references: :inviting_friend, index: true, foreign_key: true
-    add_reference :friendships , :invited , references: :invited_friend, index: true, foreign_key: true
+    add_reference :friendships , :inviting , references: :inviting_friend, index: true, foreign_key: { to_table: :users }
+    add_reference :friendships , :invited , references: :invited_friend, index: true, foreign_key: { to_table: :users }
     
   end
 end

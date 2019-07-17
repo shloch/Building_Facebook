a4a75f76f52bec81710c6ef2f6f13dcece9d2cae
pwilson77 <p.o.yirenkyi@gmail.com>
2019-07-17 14:21:46 +0000

    styled the landing_page, dashboard and profile

--- db/migrate/20190702144425_add_fields_to_user.rb
+++ db/migrate/20190702144425_add_fields_to_user.rb
@@ -1,6 +1,6 @@
 class AddFieldsToUser < ActiveRecord::Migration[5.2]
   def change
-        add_column :users, :date_of_birth, :datetime
+   add_column :users, :date_of_birth, :datetime
 	 add_column :users, :gender, :string
 	 add_column :users, :mobile, :string
   end

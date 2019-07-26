919340107fefbbaea0283644dbf41b8bd3e24539
SHEY LOUIS CHIA <shloch2007@yahoo.fr>
2019-07-26 16:21:35 +0100

    modified FRIENDSHIP bug profile page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,8 @@ class ProfilesController < ApplicationController
   def show
     @user_id = params[:id]
     @profile_user = User.find(@user_id)
-    @friends = @profile_user.invited_friends
+    #@friendships = @profile_user.invited_friends.where(status:"accepted")
+    @friendships = Friendship.get_friends_of_user(@profile_user)
     @post = Post.new 
     @post_comment = PostComment.new
     @user_posts = Post.all.includes(:author).includes(:post_comments => :user).includes(:like_posts).where(:author=> @user_id).order(id: :desc).limit(20)
